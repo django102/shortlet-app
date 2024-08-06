fae451677a2c1d21d8db138ab0f725ddde53e826
Wale K. Santos <eighteen.eighty.one@outlook.com>
2024-08-06 05:22:16 +0100

    some API security setup

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,5 +1,7 @@
 import helmet from "helmet";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+import compression from 'compression';
 
 import { Application, json, urlencoded, Request, Response } from 'express';
 import router from "../api/routes";
@@ -12,8 +14,16 @@ const corsOptions = {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 50 // limit each IP to 50 requests per windowMs
+});
+
+
 const expressConfig = (app: Application): void => {
     app.use(cors(corsOptions));
+    app.use(limiter);
+    app.use(compression());
     app.use(
         urlencoded({
             extended: true,
@@ -21,6 +31,12 @@ const expressConfig = (app: Application): void => {
     );
     app.use(json());
     app.use(helmet());
+    app.use(helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'"]
+        }
+    }));
 
     app.get('/', (req: Request, res: Response) => res.send({ message: 'Welcome' }));
     app.use("/api", router);
